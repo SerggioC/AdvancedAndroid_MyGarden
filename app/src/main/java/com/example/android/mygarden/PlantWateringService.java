5e4904f488822b1f768b4ebbffac77cfcf67d14c
SerggioC <sergiocrz@gmail.com>
2018-03-27 02:17:43 +0100

    widget updating

---
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -0,0 +1,93 @@
+package com.example.android.mygarden;
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+
+/**
+ * Created by Sergio on 27/03/2018.
+ */
+
+public class PlantWateringService extends IntentService {
+    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
+    public static final String ACTION_UPDATE_WIDGETS = "com.example.android.mygarden.action.update_widgets";
+
+
+    public PlantWateringService() {
+        super("plant_watering_service");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_WATER_PLANTS.equals(action)) {
+                handleActionWaterPlants();
+            } else if (ACTION_UPDATE_WIDGETS.equals(action)) {
+                handleActionUpdatePlantWidgets();
+            }
+        }
+    }
+
+    private void handleActionWaterPlants() {
+        Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        ContentValues contentValues = new ContentValues();
+        long timeNow = System.currentTimeMillis();
+        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+        // Update only plants that are still alive
+        getContentResolver().update(
+                PLANTS_URI,
+                contentValues,
+                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
+                new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
+    }
+
+    public static void startActionUpdatePlantsWidget(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+
+    private void handleActionUpdatePlantWidgets() {
+        //Query to get the plant that's most in need for water (last watered)
+        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        Cursor cursor = getContentResolver().query(
+                PLANT_URI,
+                null,
+                null,
+                null,
+                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME
+        );
+        // Extract the plant details
+        int imgRes = R.drawable.grass; // Default image in case our garden is empty
+        if (cursor != null && cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            int createTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+            int waterTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+            int plantTypeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
+            long timeNow = System.currentTimeMillis();
+            long wateredAt = cursor.getLong(waterTimeIndex);
+            long createdAt = cursor.getLong(createTimeIndex);
+            int plantType = cursor.getInt(plantTypeIndex);
+            cursor.close();
+            imgRes = PlantUtils.getPlantImageRes(this, timeNow - createdAt, timeNow - wateredAt, plantType);
+        }
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, PlantWidgetProvider.class));
+        //Now update all widgets
+        PlantWidgetProvider.updatePlantWidgets(this, appWidgetManager, imgRes, appWidgetIds);
+    }
+
+}
